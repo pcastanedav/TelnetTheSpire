46eb975b1e8e4f51b6fcef63d71ca042544b3bda
pcastaneda <pablo.castaneda@ymail.com>
2020-09-02 21:41:47 -0400

    Created assertOrder utility function for JargonParser tests

--- src/test/telnetthespire/commands/JargonParserTest.java
+++ src/test/telnetthespire/commands/JargonParserTest.java
@@ -1,13 +1,26 @@
 package telnetthespire.commands;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import telnetthespire.commands.parsers.StSJargonLexer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.CharBuffer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JargonParserTest {
+
+    private ByteArrayOutputStream errContent;
+    private static final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        errContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(errContent));
+    }
 
     @Test
     void prepareInput() {
@@ -18,8 +31,39 @@ class JargonParserTest {
     }
 
     @Test
-    void createLexer() {
-        StSJargonLexer lexer = JargonParser.createLexer("");
-        String[] rules = lexer.getRuleNames();
+    void emptyOrder () {
+        assertOrder(
+                "An empty order is not expected",
+                "",
+                "(order (command commandName))",
+                "mismatched input '<EOF>' expecting '.'"
+        );
+    }
+
+    @Test
+    void singleCommand () {
+        assertOrder(
+            "",
+            "play",
+            "(order (command (commandName . play)))",
+            ""
+        );
+    }
+
+    private void assertOrder (String message, String order,String expectedTree, String expectedError) {
+        assertAll(
+            message,
+            () -> {
+                ParseTree tree = JargonParser.createTree(order);
+                String parsed = tree.toStringTree(JargonParser.createParser(order));
+                assertEquals(expectedTree, parsed);
+            },
+                () -> assertTrue(errContent.toString().contains(expectedError))
+        );
+    }
+
+    @AfterAll
+    public static void restoreStreams() {
+        System.setErr(originalErr);
     }
 }
